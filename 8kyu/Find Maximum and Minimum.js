a48a4f872f23bfb459df78b81ee02603ddc31e49
Slav1k <Slav1_k@ukr.net>
2022-01-27 09:50:53 +0200

    solution Find Maximum and Minimum

---
+++ 8kyu/Find Maximum and Minimum.js
@@ -0,0 +1,14 @@
+// Your task is to make two functions, max and min that take a(n) array/vector of integers list as input and outputs, 
+// respectively, the largest and lowest number in that array/vector.
+
+
+// My solution
+function min(list){
+    
+    return  Math.min(...list);
+}
+
+function max(list){
+    
+    return  Math.max(...list);
+}
