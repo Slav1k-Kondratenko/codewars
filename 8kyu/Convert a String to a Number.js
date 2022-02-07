1b11515f00ba513bbd043b92317511df9a2e5395
Slav1_k <Slav1_k@hotmail.com>
2022-02-07 09:10:20 +0200

    solution Convert a String to a Number!

---
+++ 8kyu/Convert a String to a Number.js
@@ -0,0 +1,9 @@
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+
+// My solution
+const stringToNumber = (str) => {
+    return +str;
+}
+
+console.log(stringToNumber('123'));
