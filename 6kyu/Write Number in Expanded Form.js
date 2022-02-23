df538bfbfae9653e81c8db5d400ed3935649c9a0
Slav1_k <Slav1_k@hotmail.com>
2022-02-23 10:20:22 +0200

    solution Write Number in Expanded Form

---
+++ 6kyu/Write Number in Expanded Form.js
@@ -0,0 +1,22 @@
+// Write Number in Expanded Form
+
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12); // Should return '10 + 2'
+// expandedForm(42); // Should return '40 + 2'
+// expandedForm(70304); // Should return '70000 + 300 + 4'
+
+// NOTE: All numbers will be whole numbers greater than 0.
+
+
+// My solution
+function expandedForm(num) {
+    num = String(num);
+    let result = num[0] * Math.pow(10, num.length - 1);
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] > 0) {
+            result = `${result} + ${num[i] * Math.pow(10, (num.length - 1) - i)}`
+        }
+    }
+    return String(result);
+}
